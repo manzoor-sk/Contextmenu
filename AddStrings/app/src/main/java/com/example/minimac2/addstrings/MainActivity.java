c30c8aa5bc0cc85335c78e7c6b57d822da62e65b
minimac2 <minimac2@minimac2s-Mac-mini.local>
2016-05-06 19:38:52 +0530

    Addonebyone

---
+++ AddStrings/app/src/main/java/com/example/minimac2/addstrings/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.minimac2.addstrings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+
+    String string = "ABCD";
+    String string1 = "XYZ";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.text);
+
+//the String class is used to manipulate character strings
+
+        StringBuffer stringBuffer = new StringBuffer();
+
+
+        for (int i = 0; i < string.length(); i++) {
+            if (i < string.length())
+                stringBuffer.append(string.charAt(i));
+            if (i < string1.length())
+                stringBuffer.append(string1.charAt(i));
+        }
+        text.setText(stringBuffer.toString());
+
+    }
+
+
+}
